994856e7d1a73feadcf1c15df864e217fd78fbe2
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2024-10-09 15:23:28 +0700

    memperbaiki import

--- cekresiLION.py
+++ cekresiLION.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+from datetime import datetime
 import time
 
 def setup_driver():
